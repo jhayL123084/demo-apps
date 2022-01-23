43fc3bdec0ddd26e3c8aaf731bd0157c8013c8d7
jhayL123084 <98251171+jhayL123084@users.noreply.github.com>
2022-01-23 09:46:25 +0300

    Update main.js

--- moralis-vanilla-boilerplate/main.js
+++ moralis-vanilla-boilerplate/main.js
@@ -1,30 +1,65 @@
 /** Connect to Moralis server */
-const serverUrl = "https://xxxxx.yourserver.com:2053/server";
-const appId = "YOUR_APP_ID";
+const serverUrl = "https://sggrawk0y8lb.usemoralis.com:2053/server";
+const appId = "Jp3NwW7TNWFCzoOd9J2lYGpEO08Ox9R6h3pYzb76";
 Moralis.start({ serverUrl, appId });
+let user = Moralis.User.current();
 
 /** Add from here down */
 async function login() {
-  let user = Moralis.User.current();
   if (!user) {
    try {
       user = await Moralis.authenticate({ signingMessage: "Hello World!" })
-      console.log(user)
-      console.log(user.get('ethAddress'))
+      initApp();
    } catch(error) {
      console.log(error)
    }
   }
+  else{
+    Moralis.enableWeb3();
+    initApp();
+  }
 }
 
-async function logOut() {
-  await Moralis.User.logOut();
-  console.log("logged out");
+function initApp(){
+    document.querySelector("#app").style.display = "block";
+    document.querySelector("#submit_button").onclick = submit;
 }
 
-document.getElementById("btn-login").onclick = login;
-document.getElementById("btn-logout").onclick = logOut;
+async function submit(){
+    const input = document.querySelector('#input_image');
+    let data = input.files[0]
+    const imageFile = new Moralis.File(data.name, data)
+    await imageFile.saveIPFS();
+    let imageHash = imageFile.hash();
 
+    let metadata = {
+        name: document.querySelector('#input_name').value,
+        description: document.querySelector('#input_description').value,
+        image: "/ipfs/" + imageHash
+    }
+    console.log(metadata);
+    const jsonFile = new Moralis.File("metadata.json", {base64 : btoa(JSON.stringify(metadata))});
+    await jsonFile.saveIPFS();
+
+    let metadataHash = jsonFile.hash();
+    console.log(jsonFile.ipfs())
+    let res = await Moralis.Plugins.rarible.lazyMint({
+        chain: 'rinkeby',
+        userAddress: user.get('ethAddress'),
+        tokenType: 'ERC721',
+        tokenUri: 'ipfs://' + metadataHash,
+        royaltiesAmount: 5, // 0.05% royalty. Optional
+    })
+    console.log(res);
+    document.querySelector('#success_message').innerHTML = 
+        `NFT minted. <a href="https://rinkeby.rarible.com/token/${res.data.result.tokenAddress}:${res.data.result.tokenId}">View NFT`;
+    document.querySelector('#success_message').style.display = "block";
+    setTimeout(() => {
+        document.querySelector('#success_message').style.display = "none";
+    }, 5000)
+}
+
+login();
 /** Useful Resources  */
 
 // https://docs.moralis.io/moralis-server/users/crypto-login
